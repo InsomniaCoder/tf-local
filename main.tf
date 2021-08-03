425c64eb0f9e9a7ee1d70b68aff76b867558997d
insomniacoder <tanatloke@gmail.com>
2021-08-03 16:12:41 +0700

    use null_resource to install tekton pipeline with parameterized version

--- main.tf
+++ main.tf
@@ -1,13 +1,25 @@
-resource "kubernetes_namespace" "db-ns" {
+resource "kubernetes_namespace" "devops" {
   metadata {
-    name = "db"
+    name = "devops"
   }
 }
 
-resource "helm_release" "redis-release" {
-  name       = "local-redis-release"
+resource "helm_release" "metrics-server" {
+  count = 0
+  name       = "local-metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "redis"
-  version    = "14.8.7"
-  namespace  = kubernetes_namespace.db-ns.metadata[0].name
+  chart      = "metrics-server"
+  version    = var.metrics-server-version
+  namespace  = kubernetes_namespace.devops.metadata.0.name
+}
+
+resource "null_resource" "tekton-ci" {
+
+  depends_on = [
+    kubernetes_namespace.devops
+  ]
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/${var.tekton-ci-version}/release.yaml"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "tekton-ci-version" {
+  type    = string
+  default = "v0.26.0"
+}
+
+variable "metrics-server-version" {
+  type    = string
+  default = "5.9.2"
+}
